ce6783e630db54938cad5e1f38544b9297e7b7f3
DK\psk <psk@sitecore.net>
2017-06-21 17:04:10 -0400

    Preparation for the fix

---
+++ src/Sitecore.Support.150120/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.150120")]
+[assembly: AssemblyProduct("Sitecore.Support.150120")]
+[assembly: ComVisible(false)]
